03b45886f6ba32803a4c4ea3496cfc8cb7afaba3
Elfet <anton@elfet.ru>
2014-06-30 12:29:23 +0400

    Save color.

--- plugin/message-color/js/message-color.js
+++ plugin/message-color/js/message-color.js
@@ -1,11 +1,16 @@
-var messageColor = '#000000';
+var messageColor = localStorage.getItem('message-color');
 
 $(function () {
     var textarea = $('footer textarea');
+    textarea.css('color', messageColor);
+
     $('#colorpicker').farbtastic(function (color) {
+        localStorage.setItem('message-color', color);
         messageColor = color;
         textarea.css('color', color);
     });
+
+    $.farbtastic('#colorpicker').setColor(messageColor);
 
     $(window).on('send', function (event, message) {
         message.color = messageColor;
